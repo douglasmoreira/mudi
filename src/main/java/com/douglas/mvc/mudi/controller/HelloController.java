7fee97b0833918dd9ebdb15c0f54915605d1154a
douglas moreira <douglas.cosmo7@gmail.com>
2022-05-02 15:45:45 -0300

    alteracao no controller para usar a classe Model no lugar da classe HttpServletRequest

--- src/main/java/com/douglas/mvc/mudi/controller/HelloController.java
+++ src/main/java/com/douglas/mvc/mudi/controller/HelloController.java
@@ -1,16 +1,15 @@
 package com.douglas.mvc.mudi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String getHello(HttpServletRequest request) {
-        request.setAttribute("nome", "Mundo");
+    public String getHello(Model request) {
+        request.addAttribute("nome", "Mundo");
         return "hello";
     }
 }
