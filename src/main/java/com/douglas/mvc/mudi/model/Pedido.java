9fe4235556352c8ea1f1de0b7cd98175b0406b50
douglas moreira <douglas.cosmo7@gmail.com>
2022-05-13 08:23:33 -0300

    ajuste properties e classe Pedido para acesso ao banco de dados

--- src/main/java/com/douglas/mvc/mudi/model/Pedido.java
+++ src/main/java/com/douglas/mvc/mudi/model/Pedido.java
@@ -1,16 +1,29 @@
 package com.douglas.mvc.mudi.model;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
 public class Pedido {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nomeProduto;
     private BigDecimal valorNegociado;
     private LocalDate dataDaEntrega;
     private String urlProduto;
     private String urlImagem;
     private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getNomeProduto() {
         return nomeProduto;
