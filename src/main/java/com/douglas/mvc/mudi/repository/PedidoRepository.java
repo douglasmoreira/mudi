6c8af787c1bbf40a77188f64688091e1cbde0e7a
douglas moreira <douglas.cosmo7@gmail.com>
2022-05-13 09:02:46 -0300

    alterando classe controller para nao ter a responsabilidade de acessar o banco de dados e criando o repository para resolver esse acesso

--- src/main/java/com/douglas/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/com/douglas/mvc/mudi/repository/PedidoRepository.java
@@ -1,2 +1,24 @@
-package com.douglas.mvc.mudi.repository;public class PedidoRepository {
+package com.douglas.mvc.mudi.repository;
+
+import com.douglas.mvc.mudi.model.Pedido;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
+
+@Repository
+public class PedidoRepository {
+
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Pedido> recuperaTodosPedidos() {
+
+        Query query= entityManager.createQuery("select p from Pedido p", Pedido.class);
+        return query.getResultList();
+
+    }
 }
