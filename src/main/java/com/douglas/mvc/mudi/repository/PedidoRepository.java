cc9992feb95ed5d86b7d97150614731e878b4ef6
douglas moreira <douglas.cosmo7@gmail.com>
2022-05-13 09:09:09 -0300

    alteracao classe repository para usar o JpaRepository

--- src/main/java/com/douglas/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/com/douglas/mvc/mudi/repository/PedidoRepository.java
@@ -1,24 +1,9 @@
 package com.douglas.mvc.mudi.repository;
 
 import com.douglas.mvc.mudi.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import java.util.List;
-
 @Repository
-public class PedidoRepository {
-
-
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    public List<Pedido> recuperaTodosPedidos() {
-
-        Query query= entityManager.createQuery("select p from Pedido p", Pedido.class);
-        return query.getResultList();
-
-    }
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 }
