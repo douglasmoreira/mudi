fbcfd0d2a595598d96fa1989fa5de787af8d1ad2
douglas moreira <douglas.cosmo7@gmail.com>
2022-05-16 08:31:11 -0300

    criacao dto requisicao novo pedido

--- src/main/java/com/douglas/mvc/mudi/dto/RequisicaoNovoPedido.java
+++ src/main/java/com/douglas/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -1,2 +1,53 @@
-package com.douglas.mvc.mudi.dto;public class RequisicaoNovoPedido {
+package com.douglas.mvc.mudi.dto;
+
+import com.douglas.mvc.mudi.model.Pedido;
+
+public class RequisicaoNovoPedido {
+
+    private String nomeProduto;
+    private String urlProduto;
+    private String urlImagem;
+    private String descricao;
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getUrlProduto() {
+        return urlProduto;
+    }
+
+    public void setUrlProduto(String urlProduto) {
+        this.urlProduto = urlProduto;
+    }
+
+    public String getUrlImagem() {
+        return urlImagem;
+    }
+
+    public void setUrlImagem(String urlImagem) {
+        this.urlImagem = urlImagem;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Pedido toPedido() {
+        Pedido pedido = new Pedido();
+        pedido.setNomeProduto(nomeProduto);
+        pedido.setUrlProduto(urlProduto);
+        pedido.setUrlImagem(urlImagem);
+        pedido.setDescricao(descricao);
+
+        return pedido;
+    }
 }
