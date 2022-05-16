18a3a222bac6c4968e473ff0bb009ac082cc4d17
douglas moreira <douglas.cosmo7@gmail.com>
2022-05-16 08:10:16 -0300

    criacao controller recebe pedido para formulario

--- src/main/java/com/douglas/mvc/mudi/controller/PedidoController.java
+++ src/main/java/com/douglas/mvc/mudi/controller/PedidoController.java
@@ -1,2 +1,15 @@
-package com.douglas.mvc.mudi.controller;public class PedidoController {
+package com.douglas.mvc.mudi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("pedido")
+public class PedidoController {
+
+    @GetMapping("formulario")
+    public String formulario() {
+        return "pedido/formulario";
+    }
 }
