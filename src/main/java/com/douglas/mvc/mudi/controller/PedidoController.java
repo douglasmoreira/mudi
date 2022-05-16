3c5cdcd48d8a4a2db4eaa23c2bfe441cb126dcbc
douglas moreira <douglas.cosmo7@gmail.com>
2022-05-16 08:31:44 -0300

    criacao controller de pedido

--- src/main/java/com/douglas/mvc/mudi/controller/PedidoController.java
+++ src/main/java/com/douglas/mvc/mudi/controller/PedidoController.java
@@ -1,15 +1,30 @@
 package com.douglas.mvc.mudi.controller;
 
+import com.douglas.mvc.mudi.dto.RequisicaoNovoPedido;
+import com.douglas.mvc.mudi.model.Pedido;
+import com.douglas.mvc.mudi.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("pedido")
 public class PedidoController {
 
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
     @GetMapping("formulario")
     public String formulario() {
+        return "pedido/formulario";
+    }
+
+    @PostMapping("novo")
+    public String novoFormulario(RequisicaoNovoPedido novoPedido) {
+        Pedido pedido = novoPedido.toPedido();
+        pedidoRepository.save(pedido);
         return "pedido/formulario";
     }
 }
