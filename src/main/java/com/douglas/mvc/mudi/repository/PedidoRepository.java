1b92fa1133a3b7d35a8fe1ac39a18bdca5438bb6
douglas moreira <douglas.cosmo7@gmail.com>
2022-05-23 08:28:18 -0300

    atualizacao para usar status no backend

--- src/main/java/com/douglas/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/com/douglas/mvc/mudi/repository/PedidoRepository.java
@@ -1,9 +1,13 @@
 package com.douglas.mvc.mudi.repository;
 
 import com.douglas.mvc.mudi.model.Pedido;
+import com.douglas.mvc.mudi.model.StatusPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    List<Pedido> findByStatus(StatusPedido aguardando);
 }
