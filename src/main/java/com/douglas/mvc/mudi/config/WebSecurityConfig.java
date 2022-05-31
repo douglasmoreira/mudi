734d21b95b5635e22235e121360d9019aaa2b429
douglas moreira <douglas.cosmo7@gmail.com>
2022-05-31 08:28:50 -0300

    criando classe de configuracao de login

--- src/main/java/com/douglas/mvc/mudi/config/WebSecurityConfig.java
+++ src/main/java/com/douglas/mvc/mudi/config/WebSecurityConfig.java
@@ -1,2 +1,37 @@
-package com.douglas.mvc.mudi.config;public class WebSecurityConfig {
+package com.douglas.mvc.mudi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+            .httpBasic();
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("douglas")
+                .password("password")
+                .roles("ADM")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
 }
