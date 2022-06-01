dc9af3eb5ade79b111d5e573a4b9dc50f6e5e54a
douglas moreira <douglas.cosmo7@gmail.com>
2022-06-01 07:52:49 -0300

    configuracao do login do usuario com spring security

--- src/main/java/com/douglas/mvc/mudi/config/WebSecurityConfig.java
+++ src/main/java/com/douglas/mvc/mudi/config/WebSecurityConfig.java
@@ -17,10 +17,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
-            .anyRequest().authenticated()
-            .and()
-            .httpBasic();
+                .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll();
     }
 
     @Bean
